b917a5a3522734a173fe7d7d0b1e37144b974860
Maximiliano Agüero <aguero.maximilianojavier@gmail.com>
2022-12-04 18:13:52 -0300

    start to implement dark mode

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useEstadosGlobalesContext } from "./utils/global.context";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
+  const { theme, setTheme } = useEstadosGlobalesContext();
+
+  const handleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
 
   return (
-    <nav>
+    <nav className={theme}>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <button onClick={handleTheme}>Change theme</button>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
