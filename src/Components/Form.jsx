c3c7f35159410cfeb0fe2e623736952525eed10e
Maximiliano Agüero <aguero.maximilianojavier@gmail.com>
2022-12-05 00:36:01 -0300

    start to implement forms for users

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,12 +1,31 @@
-import React from "react";
-
+import { useState } from "react";
 
 const Form = () => {
   //Aqui deberan implementar el form completo con sus validaciones
+  const [newUser, setNewUser] = useState({
+    nombre: "",
+    email: "",
+  });
+
+  const handleInputChange = (e) => {
+    setNewUser({
+      ...newUser,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //TODO: Implementar validaciones
+    alert(`Gracias ${newUser.nombre}, te contactaremos cuando antes vía mail`);
+  };
 
   return (
     <div>
       <form>
+        <input name="nombre" type="text" onChange={handleInputChange} />
+        <input name="email" type="email" onChange={handleInputChange} />
+        <button onClick={handleSubmit}>Enviar</button>
       </form>
     </div>
   );
