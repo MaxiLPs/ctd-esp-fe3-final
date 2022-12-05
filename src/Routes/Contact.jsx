85a67d475e047a07f82a3b81bf4321c71d5b384f
Maximiliano Agüero <aguero.maximilianojavier@gmail.com>
2022-12-05 01:01:53 -0300

    Implement darkMode

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Form from '../Components/Form'
+import React from "react";
+import Form from "../Components/Form";
+import { useEstadosGlobalesContext } from "../Components/utils/global.context";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Contact = () => {
+  const { theme } = useEstadosGlobalesContext();
+
   return (
-    <div>
+    <div className={theme}>
       <h2>Want to know more?</h2>
       <p>Send us your questions and we will contact you</p>
-      <Form/>
+      <Form />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
