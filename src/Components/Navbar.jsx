a096c4b0cfaf300b7d0f472af8900f1930b7b865
Maximiliano Agüero <aguero.maximilianojavier@gmail.com>
2022-12-05 00:29:46 -0300

    Update urls according to link.jsx file

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useEstadosGlobalesContext } from "./utils/global.context";
+import { Link } from "react-router-dom";
+import { links } from "./utils/links";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
   const { theme, setTheme } = useEstadosGlobalesContext();
+  const { home, contacto, favs, dentista } = links;
 
   const handleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
@@ -13,6 +16,10 @@ const Navbar = () => {
   return (
     <nav className={theme}>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <Link to={home.path}>{home.name}</Link>
+      <Link to={contacto.path}>{contacto.name}</Link>
+      <Link to={favs.path}>{favs.name}</Link>
+      <Link to={dentista.path}>{dentista.name}</Link>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button onClick={handleTheme}>Change theme</button>
     </nav>
