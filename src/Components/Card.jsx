4735a780ddf2f2422ebd15bc3bd2a1a88ead0cdb
Maximiliano Agüero <aguero.maximilianojavier@gmail.com>
2022-12-07 19:38:27 -0300

    Put Link to the Card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { links } from "./utils/links";
 
 const Card = ({ name, username, id }) => {
   const [isFav, setIsFav] = useState(false);
@@ -18,22 +20,23 @@ const Card = ({ name, username, id }) => {
     if (favs.find((fav) => fav.id === id)) {
       //If the id is already in the array, remove it from favs
       newFavs = favs.filter((fav) => fav.id !== id);
-      setIsFav(!isFav);
     } else {
       //If the id is not in the array, add it to favs
       newFavs = [...favs, { name, username, id }];
-      setIsFav(!isFav);
     }
+    setIsFav(!isFav);
 
     localStorage.setItem("favorites", JSON.stringify(newFavs));
   };
 
   return (
     <div className="card">
-      <img src="./images/doctora.jpg" alt={username} />
-      <h4>{name}</h4>
-      <p>{username}</p>
-      {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
+      <Link to={`${links.dentista.path}/${id}`}>
+        <img src="./images/doctora.jpg" alt={username} />
+        <h4>{name}</h4>
+        <p>{username}</p>
+        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
+      </Link>
       <button onClick={handleFavs} className="favButton">
         <span className={`material-symbols-outlined ${isFav ? "fav" : ""}`}>
           favorite
