ee4bd2bebaced956aae580fe3fcf15c0464ad1dd
Maximiliano Agüero <aguero.maximilianojavier@gmail.com>
2022-12-07 19:38:13 -0300

    Show the favs

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "../Components/Card";
 import { useEstadosGlobalesContext } from "../Components/utils/global.context";
 
@@ -7,12 +7,29 @@ import { useEstadosGlobalesContext } from "../Components/utils/global.context";
 const Favs = () => {
   const { theme } = useEstadosGlobalesContext();
 
+  const favs = JSON.parse(localStorage.getItem("favorites")) || [];
+
+  const handleFavs = () => {
+    newFavs = favs.filter((fav) => fav.id !== id);
+    localStorage.setItem("favorites", JSON.stringify(newFavs));
+  };
+
   return (
     <div className={theme}>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
-        {/* este componente debe consumir los destacados del localStorage */}
-        {/* Deberan renderizar una Card por cada uno de ellos */}
+        {favs.length ? (
+          favs.map((fav) => (
+            <Card
+              key={fav.id}
+              name={fav.name}
+              username={fav.username}
+              id={fav.id}
+            />
+          ))
+        ) : (
+          <p>No hay favoritos</p>
+        )}
       </div>
     </div>
   );
