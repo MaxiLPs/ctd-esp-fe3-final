0ad9a8dabc9c3c72988acecf9146e5d54244743e
Maximiliano Agüero <aguero.maximilianojavier@gmail.com>
2022-12-08 00:07:49 -0300

    add simple validation to contact form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -7,6 +7,8 @@ const Form = () => {
     email: "",
   });
 
+  const [errors, setErrors] = useState({});
+
   const handleInputChange = (e) => {
     setNewUser({
       ...newUser,
@@ -14,8 +16,40 @@ const Form = () => {
     });
   };
 
+  const validate = () => {
+    const emailRegex =
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+
+    let nombreError = "";
+    let emailError = "";
+
+    if (!newUser.nombre || newUser.nombre.length < 5) {
+      nombreError = "El nombre es requerido y debe tener al menos 5 caracteres";
+    }
+
+    if (!newUser.email || !emailRegex.test(newUser.email)) {
+      emailError = "El email debe ser válido";
+    }
+
+    if (nombreError || emailError) {
+      setErrors({ nombreError, emailError });
+      return false;
+    }
+
+    return true;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const isValid = validate();
+
+    console.log(errors);
+
+    if (!isValid) {
+      return;
+    }
+
     //TODO: Implementar validaciones
     alert(`Gracias ${newUser.nombre}, te contactaremos cuando antes vía mail`);
   };
@@ -24,7 +58,9 @@ const Form = () => {
     <div>
       <form>
         <input name="nombre" type="text" onChange={handleInputChange} />
+        {errors.nombreError && <label>{errors.nombreError}</label>}
         <input name="email" type="email" onChange={handleInputChange} />
+        {errors.emailError && <label>{errors.emailError}</label>}
         <button onClick={handleSubmit}>Enviar</button>
       </form>
     </div>
